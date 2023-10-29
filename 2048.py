eab347119e334ca61a5be1e63e079d93fa4617f4
Milad Valizadeh <miladvalizadeh2003@gmail.com>
2023-10-29 10:52:25 +0330

    Start...

---
+++ 2048.py
@@ -0,0 +1,15 @@
+import random
+
+
+table = [
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0]
+]
+
+
+print("""Press 'A' to MOVE LEFT
+Press 'S' to MOVE DOWN
+Press 'D' to MOVE RIGHT
+Press 'W' to MOVE UP""")
