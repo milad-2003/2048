6336ac3ddda8fa7a9dd666a9a5d468c7849d2c7f
milad-2003 <miladvalizadeh2003@gmail.com>
2023-10-29 11:11:10 +0330

    Adding "add_new_2" function.

--- 2048.py
+++ 2048.py
@@ -13,3 +13,14 @@ print("""Press 'A' to MOVE LEFT
 Press 'S' to MOVE DOWN
 Press 'D' to MOVE RIGHT
 Press 'W' to MOVE UP""")
+
+
+def add_new_2():
+    row = random.randint(0, 3)
+    column = random.randint(0, 3)
+    while 0 not in table[row]:
+        row = random.randint(0, 3)
+    while table[row][column] != 0:
+        column = random.randint(0, 3)
+    table[row][column] = 2
+
