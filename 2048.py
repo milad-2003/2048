4b769fdabf6ed8737f9709b30c20bdb23d80796d
milad-2003 <miladvalizadeh2003@gmail.com>
2023-10-29 11:12:05 +0330

    Adding "status" function.

--- 2048.py
+++ 2048.py
@@ -24,3 +24,28 @@ def add_new_2():
         column = random.randint(0, 3)
     table[row][column] = 2
 
+
+def status():
+    for i in range(0, 4):
+        for j in range(0, 4):
+            if table[i][j] == 2048:
+                return "You won!"
+            
+    for i in range(0, 4):
+        for j in range(0, 4):
+            if table[i][j] == 0:
+                return "Game not over yet..."
+
+    for i in range(0, 4):
+        for j in range(1, 4):
+            if table[i][j - 1] == table[i][j]:
+                return "Game not over yet..."
+
+    for i in range(1, 4):
+        for j in range(0, 4):
+
+            if table[i - 1][j] == table[i][j]:
+                return "Game not over yet..."
+
+    return "Game over!"
+
