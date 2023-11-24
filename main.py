0f72c6db10b25d256fb715a0732acd5597569e5d
milad-2003 <miladvalizadeh2003@gmail.com>
2023-11-24 03:59:28 +0330

    Adding various colors to print() functions.

--- main.py
+++ main.py
@@ -16,6 +16,30 @@ table_copy = []
 
 score = 0
 
+color_dict = {
+    2: (0, 41),
+    4: (0, 42),
+    8: (0, 43),
+    16: (0, 44),
+    32: (0, 45),
+    64: (0, 46),
+    128: (0, 47),
+    256: (1, 41),
+    512: (1, 42),
+    1024: (1, 43),
+    2048: (1, 44),
+    4096: (1, 45),
+    8192: (1, 46),
+    16384: (1, 47),
+    32768: (7, 41),
+    65536: (7, 42),
+    131072: (7, 43),
+    262144: (7, 44),
+    524288: (7, 45),
+    1048576: (7, 46),
+    2097152: (7, 47)
+}
+
 
 def add_new_2(arr):
     for i in range(len(arr)):
@@ -54,7 +78,7 @@ def user():
     input_list = ['A', 'S', 'D', 'W', 'Q', 'a', 's', 'd', 'w', 'q']
     user_input = str(getch())[2]
     while user_input not in input_list:
-        print("You can only select 'A', 'S', 'D' or 'W': \nPress 'Q' to exit: ")
+        print("\033[1;31;40mYou can only select 'A', 'S', 'D' or 'W': \nPress 'Q' to exit: ")
         user_input = str(getch())[2]
 
     return user_input.lower()
@@ -81,21 +105,21 @@ while not game_over(table):
     else:
         command("clear")
     
-    print("""
+    print("""\033[0;32;40m
 Press 'A', 'S', 'D' or 'W' to move
 Press 'Q' to exit
 """)
 
-    print(f"Score: {score}")
+    print(f"\033[1;34;40mScore: {score}")
 
     for i in table:
-        print("\n--------------------------------")
+        print("\033[0;37;40m\n--------------------------------")
         for j in i:
             if j:
-                print(j, "\t", end="")
+                print(f"\033[{color_dict[j][0]};37;{color_dict[j][1]}m{j}", "\033[0;37;40m\t", end="")
             else:
                 print("", "\t", end="")
-    print("\n================================\n")
+    print("\033[1;35;40m\n================================\n")
     
     # Making a copy of the table to know if it was changed in the next iteration
     table_copy = copy(table)
@@ -112,4 +136,4 @@ Press 'Q' to exit
         case "q":
             break
 
-print(f"Game over!\nScore: {score}")
+print(f"\033[0;33;40mGame over!\nScore: {score}")
